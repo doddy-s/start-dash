fce929531a52039bc315e88270b15ca87bfb51bf
doddy-s <doddy.s@protonmail.com>
2026-03-09 11:43:04 +0700

    init: init boilerplate

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '../src/generated/prisma/client.js'
+
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+const prisma = new PrismaClient({ adapter })
+
+async function main() {}
+
+main()
+  .catch((e) => {
+    console.error('❌ Error seeding database:', e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
